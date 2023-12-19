774942b86f5948392c9df57c85d21108b6186e1d
duongtranngoc <duongtn0306@gmail.com>
2023-12-20 02:18:17 +0700

    edit the functions in the controller and the search roles feature

--- app/Http/Controllers/admin/RoleController.php
+++ app/Http/Controllers/admin/RoleController.php
@@ -8,78 +8,118 @@ use App\Models\Role;
 
 class RoleController extends Controller
 {
-    public function getAllRole()
+    public function getAllRoles()
     {
-        $roles = Role::select('role_id', 'role_name', 'role_description')
-            ->latest()
-            ->get();
+        $roles = Role::all();
         return response()->json($roles);
     }
 
     public function addRole(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'role_name' => 'required|string|max:255',
             'role_description' => 'nullable|string',
         ]);
 
-        $role = Role::create([
-            'role_name' => $request->input('role_name'),
-            'role_description' => $request->input('role_description'),
-        ]);
-
-        return response()->json([
-            'message' => 'Thêm Role thành công!',
-            'data' => $role,
-        ], 200);
+        try {
+            $role = Role::create($data);
+            return response()->json([
+                'message' => 'Thêm role thành công!',
+                'data' => $role,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Thêm role không thành công.',
+            ], 500);
+        }
     }
 
     public function editRole(Request $request)
     {
         try {
-            $request->validate([
-                'role_id' => 'required|exists:roles,role_id',
+            $data = $request->validate([
+                'role_id' => 'required|int',
                 'role_name' => 'required|string|max:255',
                 'role_description' => 'nullable|string',
             ]);
 
-            $roleId = $request->input('role_id');
-            $role = Role::find($roleId);
+            $role = Role::find($data['role_id']);
+
+            if (!$role) {
+                return response()->json([
+                    'message' => 'Không tìm thấy role.',
+                ], 404);
+            }
 
             $role->update([
-                'role_name' => $request->input('role_name'),
-                'role_description' => $request->input('role_description'),
+                'role_name' => $data['role_name'],
+                'role_description' => $data['role_description'],
             ]);
 
             return response()->json([
-                'message' => 'Chỉnh sửa Role thành công!',
+                'message' => 'Chỉnh sửa role thành công!',
                 'data' => $role,
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
-                'message' => 'Không tìm thấy Role.',
-            ], 404);
+                'message' => 'Đã có lỗi xảy ra trong quá trình chỉnh sửa role.',
+            ], 500);
         }
     }
 
     public function getRoleDetail(Request $request)
     {
+        $data = $request->validate([
+            'role_id' => 'required|int',
+        ]);
+
         try {
-            $request->validate([
-                'role_id' => 'required|exists:roles,role_id',
-            ]);
+            $role = Role::find($data['role_id']);
 
-            $roleId = $request->input('role_id');
-            $role = Role::findOrFail($roleId);
+            if (!$role) {
+                return response()->json([
+                    'message' => 'Không tìm thấy role.',
+                ], 404);
+            }
 
-            return response()->json([
-                'message' => 'Lấy chi tiết Role thành công!',
-                'data' => $role,
-            ], 200);
+            return response()->json($role);
         } catch (\Exception $e) {
             return response()->json([
-                'message' => 'Không tìm thấy Role.',
-            ], 404);
+                'message' => 'Đã có lỗi xảy ra trong quá trình lấy thông tin role.',
+            ], 500);
+        }
+    }
+
+    public function searchRoles(Request $request)
+    {
+        $data = $request->validate([
+            'keySearch' => 'nullable|string|max:255',
+        ]);
+
+        try {
+            $query = Role::select('role_id', 'role_name', 'role_description')
+                ->when($data['keySearch'], function ($query) use ($data) {
+                    $query->where(function ($q) use ($data) {
+                        $q->where('role_name', 'like', '%' . $data['keySearch'] . '%')
+                            ->orWhere('role_description', 'like', '%' . $data['keySearch'] . '%');
+                    });
+                })
+                ->latest()
+                ->get();
+
+            $roles = $query->map(function ($role) {
+                return [
+                    'role_id' => $role->role_id,
+                    'role_name' => $role->role_name,
+                    'role_description' => $role->role_description,
+                ];
+            });
+
+            return response()->json($roles);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Đã có lỗi xảy ra trong quá trình tìm kiếm.',
+            ], 500);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\admin\RoleController;
 
 Route::group(['prefix' => 'admin'], function () {
     Route::group(['prefix' => 'role'], function () {
-        Route::post('get-all', [RoleController::class, 'getAllRole']);
+        Route::post('get-all', [RoleController::class, 'getAllRoles']);
         Route::post('create', [RoleController::class, 'addRole']);
         Route::post('update', [RoleController::class, 'editRole']);
         Route::post('detail', [RoleController::class, 'getRoleDetail']);
+        Route::post('search', [RoleController::class, 'searchRoles']);
     });
 });
